fff7c7c02315e6855514576bc1a42b7d98f8368d
Muratibmtal <muratibmtal@gmail.com>
2021-09-29 23:21:28 +0300

    grgr

--- src/area/jıol.java
+++ src/area/jıol.java
@@ -9,6 +9,7 @@ public class jýol {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String projectName="Türkiye ve Bölgeleri";
+		System.out.println("Türkiye ve Bölgeleri");
 		System.out.println("Marmara Bölgesi");
 		System.out.println("Karadeniz Bölgesi");
 		System.out.println("Ege Bölgesi");
