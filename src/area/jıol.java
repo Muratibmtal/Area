ac1c35cbe06fc7be13b13996ef59a0eeb5b5eace
Muratibmtal <muratibmtal@gmail.com>
2021-09-29 22:31:40 +0300

    Initial commit

---
+++ src/area/jıol.java
@@ -0,0 +1,25 @@
+package area;
+
+public class jýol {
+
+	public jýol() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String projectName="Türkiye ve Bölgeleri";
+		System.out.println("Marmara Bölgesi");
+		System.out.println("Karadeniz Bölgesi");
+		System.out.println("Ege Bölgesi");
+		System.out.println("Akdeniz Bölgesi");
+		System.out.println("Ýç Anadolu Bölgesi");
+		System.out.println("Doðu Anadolu Bölgesi");
+		System.out.println("Güneydoðu Anadolu Bölgesi");
+		System.out.println("SON :)");
+		
+		
+		
+	}
+
+}
